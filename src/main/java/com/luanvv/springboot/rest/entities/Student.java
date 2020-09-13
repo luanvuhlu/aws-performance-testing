41c590ce1c937b9042706d73eb85795686a897e8
DESKTOP-LUAN\luan vu <luanvuhlu@gmail.com>
2020-09-13 14:35:51 +0700

    change to auto_increment

--- src/main/java/com/luanvv/springboot/rest/entities/Student.java
+++ src/main/java/com/luanvv/springboot/rest/entities/Student.java
@@ -2,12 +2,14 @@ package com.luanvv.springboot.rest.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Student {
+
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 
