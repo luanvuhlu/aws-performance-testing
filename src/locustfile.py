8672f7fae0641ce77bc917fea7eec65c2748bd06
DESKTOP-LUAN\luan vu <luanvuhlu@gmail.com>
2020-09-13 17:16:31 +0700

    Remove devtool

--- src/locustfile.py
+++ src/locustfile.py
@@ -1,5 +1,21 @@
 # locustfile.py
+import random
+import string
+import json
+import resource
 from locust import HttpUser, task
+
+
+try:
+    resource.setrlimit(resource.RLIMIT_NOFILE, (1000000, 1000000))
+except:
+    print("Couldn't raise resource limit")
+
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
 
 class WebsiteUser(HttpUser):
     min_wait = 5000
@@ -15,4 +31,12 @@ class WebsiteUser(HttpUser):
 
     @task(2)
     def get_one(self):
-        self.client.get('/student/10001')
+        self.client.get('/student/' + str(random.randint(1, 1000)))
+
+    
+
+    @task(2)
+    def update_one(self):
+        payload = {'name': get_random_string(1)}
+        headers = {'content-type': 'application/json'}
+        self.client.put('/students/' + str(random.randint(1, 1000)), headers=headers, data=json.dumps(payload))
